271252f0fa96a4ca26b122cb7b1cdb03ee15d0fb
kscript <1258@live.cn>
2019-12-25 14:46:52 +0800

    添加axios

---
+++ src/axios.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosError, AxiosResponse, AxiosRequestConfig } from 'axios';
+import * as qs from 'qs';
+
+interface anyObject<T = any> {
+    [prop: string]: T;
+}
+
+const $axios = axios.create({
+    baseURL: '/',
+    timeout: 15000,
+    transformRequest: [(data: anyObject) => {
+        return data instanceof FormData ? data : qs.stringify(data);
+    }],
+});
+
+$axios.interceptors.request.use((config: AxiosRequestConfig) => {
+    return config;
+}, (err: AxiosError) => {
+    return Promise.reject(err);
+});
+
+$axios.interceptors.response.use((response: AxiosResponse<any>) => {
+    return Promise.resolve(response);
+}, (err: AxiosError) => {
+    return Promise.reject(err);
+});
+
+export default $axios;
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,7 +1,8 @@
-import { ActionTree } from "vuex";
-import StoreData from "@/store/state";
+import { ActionTree } from 'vuex';
+import StoreData from '@/store/state';
+import axios from '@/axios';
 const actions: ActionTree<StoreData, StoreData> = {
-  account({ state, commit, getters, dispatch }, value: string) {
+  account ({ state, commit, getters, dispatch }, value: string) {
     commit('account', value);
   }
 }
