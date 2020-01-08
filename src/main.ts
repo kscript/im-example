b28e99f9093f4004d0c46e3369decacc91ee1b5e
kscript <1258@live.cn>
2020-01-08 16:25:52 +0800

    添加element-ui

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import eventBus from '@/eventBus'
-
+import ELEMENT from 'element-ui'
 Vue.config.productionTip = false
+Vue.use(ELEMENT)
 Vue.use(eventBus)
 new Vue({
   router,
